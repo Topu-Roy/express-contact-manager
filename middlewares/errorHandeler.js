c6376f4e4b96cbedbc55609b5608a93344973362
Topu Roy <topuroy143333@gmail.com.com>
2023-05-06 19:00:25 +0600

    Added custom error handeler middleware Compleated

--- middlewares/errorHandeler.js
+++ middlewares/errorHandeler.js
@@ -1,9 +1,46 @@
+const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
   const statsCode = res.statusCode ? res.statusCode : 500;
-  res.json({
-    message: err.message,
-    stackTrace: err.stack,
-  });
+
+  switch (statsCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validations Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+    default:
+      console.log("There was no error, all good!");
+      break;
+  }
 };
 
 module.exports = errorHandler;
