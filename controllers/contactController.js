508a4e5d0c7faf2ecc453b7574b492bc0dbb479c
Topu Roy <topuroy143333@gmail.com.com>
2023-05-06 19:11:11 +0600

    Added Express async handler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,44 @@
+const asyncHandler = require("express-async-handler");
+
 //* method : get
 //* route : /api/contacts
 //* access : public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler(async (req, res) => {
   res.json({ message: "get all contacts" });
-};
+});
 
 //* method : post
 //* route : /api/contacts
 //* access : public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are required !");
   }
   res.json({ message: "create new contact" });
-};
+});
 
 //* method : get
 //* route : /api/contacts/:id
 //* access : public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.json({ message: `Get contact for id ${req.params.id}` });
-};
+});
 
 //* method : put
 //* route : /api/contacts/:id
 //* access : private
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.json({ message: `Update contact for id ${req.params.id}` });
-};
+});
 
 //* method : delete
 //* route : /api/contacts/:id
 //* access : priv
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.json({ message: `Delete contact for id ${req.params.id}` });
-};
+});
 
 module.exports = {
   getAllContacts,
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-const errorHandler = require("./middlewares/errorHandeler");
+const errorHandler = require("./middleware/errorHandler");
 
 const port = process.env.PORT || 5000;
 const app = express();
